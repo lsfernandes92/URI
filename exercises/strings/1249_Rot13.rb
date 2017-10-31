ea2dc88b3a79788cbb80f0cb6b3667b64046acc8
lsfernandes92 <lsfernandes92@gmail.com>
2017-10-31 14:20:43 -0200

    Little refatoration :)

--- exercises/strings/1249_Rot13.rb
+++ exercises/strings/1249_Rot13.rb
@@ -4,57 +4,33 @@
 def ask_for_message
   puts "Hello hunny bunny! Type the message you wanna to be coded:"
   secret_message = gets(nil).strip
-  each_letter_of_message secret_message
 end
 
 def print_result(result)
   result.each { |r| print r }
+  puts
 end
 
-def each_letter_of_message(message)
-  each_letter_message = []
-  chars = message.split('')
-  chars.each { |letter| each_letter_message << letter }
-  each_letter_message
-end
-
-def letter_coded(letter)
-  index_of_letter = @alphabet_downcase.index(letter) + 1
-  position_to_coded = index_of_letter + 13
-  alphabet_size = @alphabet_downcase.count
-  if (position_to_coded) > alphabet_size
-    diference = alphabet_size - index_of_letter
-    new_position = 12 - diference
-    @alphabet_downcase[new_position]
+def code_letter(letter)
+  has_letter_downcase = @alphabet_upsidedown.key?(letter)
+  has_letter_upcase = @alphabet_upsidedown.key?(letter.downcase)
+  if has_letter_downcase
+    @alphabet_upsidedown[letter]
+  elsif has_letter_upcase
+    @alphabet_upsidedown[letter.downcase].upcase
   else
-    new_position = index_of_letter + 12
-    @alphabet_downcase[new_position]
+    letter
   end
 end
 
 def code_message(message)
   coded_message = []
-  message.each do |letter_message|
-    add_symbol = true
-    letter_downcase_include = @alphabet_downcase.include? letter_message
-    letter_upcase_include = @alphabet_upcase.include? letter_message
-    if letter_downcase_include
-      coded_message << letter_coded(letter_message)
-      add_symbol = false
-    end
-    if letter_upcase_include
-      coded_message << letter_coded(letter_message.downcase).upcase
-      next
-    end
-    coded_message << letter_message if add_symbol
-  end
+  message.chars.each { |letter_message| coded_message << code_letter(letter_message) }
   coded_message
 end
 
-range_words_downcase = "a".."z"
-range_words_upcase = "A".."Z"
-@alphabet_downcase = range_words_downcase.to_a
-@alphabet_upcase = range_words_upcase.to_a
+@alphabet = Array("a".."z")
+@alphabet_upsidedown = @alphabet.zip(@alphabet.rotate(13)).to_h
 
 secret_message = ask_for_message
 result = code_message secret_message
